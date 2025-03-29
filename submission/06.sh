7664bf4330d61062dce6cd4b67007effc0d25018
Yussif Muniru <yussifmuniruwulon@gmail.com>
2025-03-29 15:31:25 +0000

    Fix 6

--- submission/06.sh
+++ submission/06.sh
@@ -5,6 +5,6 @@
 PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
 HASH=$(bitcoin-cli -regtest decodepsbt "$PSBT" | jq -r '.tx.hash')
-echo "✅ TXID: $HASH"
+echo $HASH
 
 
